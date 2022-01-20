e320a7bd4feccca14e00cf7911f43167f65f0d7a
Viktoriia Huk <relax16011991@gmail.com>
2022-01-20 23:35:15 +0200

    fix

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -14,14 +14,6 @@ export const Overlay = styled.div`
 `;
 
 export const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.8);
-  z-index: 1200;
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
 `;
